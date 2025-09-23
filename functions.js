bcee4bbb064ec99b30763b4727ec89eaec1c2ffd
Daria Vasylieva <daria.vasylieva@ivkhk.ee>
2025-09-23 19:57:40 +0300

    New function

--- functions.js
+++ functions.js
@@ -28,10 +28,98 @@ const multiply = (a, b) => a * b;
 console.log(sumArrow(9, 8));
 console.log(multiply(9,8));
 
+//Пример с "this"
+const person = {
+    name: "Oleg",
+    greetRegular: function() {
+
+      //this - здесь это обьект person
+        console.log(`Hello from ${this.name} (regular)` );
+    }, 
+    greetArrow: () => {
+        //у стрелочной функции нет своего this (контекста)
+        console.log(`Hello from ${this.name} arrow`);
+    }
+}
+person.greetRegular();
+person.greetArrow();
 
 
+//ПАРАМЕТРЫ ФУНКЦИИ
 
 
+console.log("---Параметры функции---");
+//Параметры по-умолчанию (default params)
+function greet(name = "Somebody") {
+    console.log(`Welcome ${name}!` );
+
+}
+greet("Anna");
+greet();
+
+//REST-params
+//Собирают все переданные аргументы (параметры) в массив
+
+function sumAll (...numbers) {
+let total = 0;
+for (const num of numbers) {
+    total += num;
+}
+return total;
+
+}
+console.log(`Sum 1,2,3,4: `, sumAll(1,2,3,4));
+
+
+//call-back -функции
+//это функция, переданная в другую функцию
+//в качестве аргумента
+//классический пример callback
+setTimeout(() => {
+console.log(`это сообщение появится через 2 сек`);
+
+}, 2000);
+
+function operate (a , b, operationCallback) {
+    return operationCallback(a,b);
+
+}
+const add = (x,y) => x + y;
+const subtract = (x,y) => x - y;
+console.log("Вызов колбеком add: " , operate(10,5, add));
+console.log("Вызов колбеком su: " , operate(10,5, subtract));
+
+console.log(`---Замыкания---`);
+
+//Замыкания - комбинация функци и ее окружения.
+//проще - внутренняя функция "помнит" переменные внешней функции
+//даже после того, как внешняя завершила работу.
+
+function createCounter() {
+    let count = 0; //эта переменная в лексич. окружении 
+    //внутренняя функция, которая будет возвращена
+    return function() {
+        count++;
+        console.log(count);
+        return count;
+        
+    };
+}
+const counter1 = createCounter();
+counter1();
+counter1();//Помнит предыдущее значение.
+const counter2 = createCounter();
+counter2();
+
+console.log("---самовызывающаяся функция----");
+//IIFE - Immediately Invoked Function Expression
+
+(function() {
+    const secretMessage = "Это сообщение не доступно глобально";
+console.log(secretMessage);
+
+
+})();
 
 
 
